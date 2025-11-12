3b32f3e73b75d17c9224362f4281418d6557e3dc
kevinmurphy4533 <kevinmurphy4533@gmail.com>
2025-11-11 20:51:10 -0500

    GUI Size Fix
    Changes the vertical size of the GUI to work with 1080p displays.

--- JavaFXGUIDemo/FMRGUI/src/main/java/fmrcrawler/fmrgui/FMRGUIApplication.java
+++ JavaFXGUIDemo/FMRGUI/src/main/java/fmrcrawler/fmrgui/FMRGUIApplication.java
@@ -11,8 +11,8 @@ public class FMRGUIApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(FMRGUIApplication.class.getResource("FMRGUI.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1270, 1000);
-        stage.setTitle("FMR Report Display");
+        Scene scene = new Scene(fxmlLoader.load(), 1270, 475);
+        stage.setTitle("Report Display System");
         stage.setScene(scene);
         stage.show();
     }
